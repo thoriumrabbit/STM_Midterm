2fdb5d99c6a875127d31ec36761057345c2adab2
thoriumrabbit <bincholon02@gmail.com>
2022-11-05 20:58:04 +0700

    final update

--- Extra/extra.c
+++ Extra/extra.c
@@ -5,25 +5,25 @@
 char* unitof10pow3(int i){
     switch (i){
     case -3:
-        return "+ nano";
+        return " nano";
         break;
     case -2:
-        return "+ micro";
+        return " micro";
         break;
     case -1:
-        return "+ mili";
+        return " mili";
         break;
     case 0:
         return " ";
         break;
     case 1:
-        return "+ Kilo";
+        return " Kilo";
         break;
     case 2:
-        return "+ Mega";
+        return " Mega";
         break;
     case 3:
-        return "+ Giga";
+        return " Giga";
         break;
     }
 }
@@ -32,7 +32,7 @@ void suffixWithUnit(double number){
     int i = 0;
     if (number == 0);
     else if (number > 1000) {
-        while(natural_part > 1){
+        while(natural_part > 1 && i < 3){
             natural_part = natural_part / 1000;
             if (natural_part < 1) {
                 natural_part *= 1000;
@@ -42,7 +42,7 @@ void suffixWithUnit(double number){
         }
     }
     else if (number < 1) {
-        while (natural_part < 1){
+        while (natural_part < 1 && i > -3){
             natural_part = natural_part * 1000;
             if (natural_part > 100){
                 natural_part /= 1000;
@@ -51,12 +51,16 @@ void suffixWithUnit(double number){
             i--;
         }
     }
-    printf("%lf %s",natural_part, unitof10pow3(i));
+    printf("%.4f%s\n",natural_part, unitof10pow3(i));
 }
 
 int main()
 {
-    suffixWithUnit(0.1);
+    suffixWithUnit(0.00000000032);
+    suffixWithUnit(0.0000062);
+    suffixWithUnit(0.000000032);
+    suffixWithUnit(0.002);
+    suffixWithUnit(152423);
     return 0;
 }
 
